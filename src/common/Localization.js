f89c54403531ed8f6d2986e2a7b7219aedf6392c
Abhijit Roy <roybolpurabhijit@gmail.com>
2022-01-23 11:21:13 +0530

    Update Localization.js

--- src/common/Localization.js
+++ src/common/Localization.js
@@ -1,7 +1,7 @@
 const Localization = {
   ITEMS_TEXT : 'Items',
   FOUR_NOT_FOUR_TEXT: '404',
-  BACT_TO_LIST_TEXT: 'Back to list',
+  BACT_TO_LIST_TEXT: 'Back to cart',
   PAGE_NOT_FOUND_HEADING_TEXT: 'Oops! Page Not Be Found',
   PAGE_NOT_FOUND_DESCRIPTION_TEXT: 'Sorry but the page you are looking for does not exists, have been removed, name changed or temporarily unavailable.',
   NO_PRODUCT_AVAILABLE_TEXT: 'No items available in cart!'
